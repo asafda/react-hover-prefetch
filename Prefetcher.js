b381cf9e968546157e6accd765701580b4b40866
Asaf David <asaf.d@fiverr.com>
2017-03-20 09:14:22 +0200

    component

---
+++ Prefetcher.js
@@ -0,0 +1,41 @@
+const Prefetcher = ({ onClickAction, prefetch, children }) => {
+    let prefetching = false,
+        fetching = false,
+        result = undefined;
+
+    const handleMouneEnter = () => {
+        if (result || fetching || prefetching) {
+            return;
+        }
+
+        prefetching = true;
+        console.log('fetching...');
+
+        prefetch()
+            .then((response) => {
+                prefetching = false;
+                result = response;
+
+                fetching && onClickAction(result);
+            });
+    }
+
+    const handleChildClicked = () => {
+        fetching = true;
+
+        if (result) {
+            onClickAction(result);
+        } else if (prefetching) {
+            return;
+        } else {
+            prefetch();
+        }
+    }
+
+    return React.cloneElement(children, {
+        onClick: handleChildClicked,
+        onMouseEnter: handleMouneEnter
+    })
+}
+
+export default Prefetcher;
